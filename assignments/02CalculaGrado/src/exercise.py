6e8d9ab817a13608b870a3506d7e73febe723d14
MaJo-Tejeda <88874353+MaJo-Tejeda@users.noreply.github.com>
2021-08-30 22:39:12 -0700

    Update and rename exercise.py to grado.py

--- assignments/02CalculaGrado/src/exercise.py
+++
@@ -1,7 +0,0 @@
-def main():
-    #escribe tu código abajo de esta línea
-    x = float(input("Ingresa Un valor entre 0.0 y 1.0: "))
-    print(calcula_grado(x))
-
-if __name__=='__main__':
-    main()
